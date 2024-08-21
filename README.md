317c4bdbf5f0fa5fa33b65ea9cedd955cb00c1b4
Harminder Virk <virk.officials@gmail.com>
2024-08-21 15:45:13 +0530

    feat: initial working commit

---
+++ README.md
@@ -0,0 +1,98 @@
+# @adonisjs/eslint-config
+> Compatible with ESLint 9.0 and Prettier 3.0
+
+<hr>
+<br />
+
+<div align="center">
+  <h3>ESLint presets used by the AdonisJS core team</h3>
+  <p>The <code>adonisjs/eslint-config</code> ships with the default set of ESLint rules used by the AdonisJS core team. The presets are tuned to work alongside Prettier.</p>
+</div>
+
+<br />
+
+<div align="center">
+
+[![gh-workflow-image]][gh-workflow-url] [![npm-image]][npm-url] ![][typescript-image] [![license-image]][license-url]
+
+</div>
+
+## Installation
+
+Install the package from the npm registry.
+
+```sh
+npm i -D @adonisjs/eslint-config
+
+# Install peer dependencies
+npm i -D eslint prettier
+```
+
+## Usage
+
+After installation, use one of the following presets depending on the nature of your application/library.
+
+**For package development**: Use the following preset when developing a package
+
+```ts
+// eslint.config.js
+import { configPkg } from '@adonisjs/eslint-config'
+export default configPkg()
+```
+
+**For app development**: Use the following preset when developing an AdonisJS application
+
+```ts
+// eslint.config.js
+import { configApp } from '@adonisjs/eslint-config'
+export default configApp()
+```
+
+## Adding additional config blocks
+You can pass additional config blocks as multiple arguments to one of the preset functions.
+
+```ts
+import { configApp, INCLUDE_LIST, IGNORE_LIST } from '@adonisjs/eslint-config'
+
+export default configApp(
+  {
+ name: 'Custom config',
+ files: INCLUDE_LIST,
+ ignores: IGNORE_LIST,
+ plugins: {
+      // ESLint plugins go here
+    },
+ rules: {
+      // ESLint rules go here
+    },
+  },
+)
+```
+
+<div align="center">
+  <h3>
+    <a href="https://adonisjs.com">
+      Website
+    </a>
+    <span> | </span>
+    <a href="https://docs.adonisjs.com">
+      Guides
+    </a>
+    <span> | </span>
+    <a href="https://github.com/adonisjs/.github/blob/main/docs/CONTRIBUTING.md">
+      Contributing
+    </a>
+  </h3>
+</div>
+
+<div align="center">
+  <sub>Built with ❤︎ by <a href="https://github.com/Julien-R44">Julien Ripouteau</a> and <a href="https://github.com/thetutlage">Harminder Virk</a>
+</div>
+
+[gh-workflow-image]: https://img.shields.io/github/actions/workflow/status/adonisjs/eslint-config/checks.yml?branch=develop&label=Tests&style=for-the-badge
+[gh-workflow-url]: https://github.com/adonisjs/eslint-config/actions/workflows/checks.yml 'Github action'
+[npm-image]: https://img.shields.io/npm/v/@adonisjs/eslint-config/latest.svg?style=for-the-badge&logo=npm
+[npm-url]: https://www.npmjs.com/package/@adonisjs/eslint-config/v/latest 'npm'
+[typescript-image]: https://img.shields.io/badge/Typescript-294E80.svg?style=for-the-badge&logo=typescript
+[license-url]: LICENSE.md
+[license-image]: https://img.shields.io/github/license/adonisjs/adonis-framework?style=for-the-badge
---
+++ index.js
@@ -0,0 +1,239 @@
+/*
+ * @adonisjs/eslint-config
+ *
+ * (c) AdonisJS
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import tseslint from 'typescript-eslint'
+import unicorn from 'eslint-plugin-unicorn'
+import stylistic from '@stylistic/eslint-plugin-ts'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+
+/**
+ * Default list of files to include
+ */
+export const INCLUDE_LIST = ['**/*.ts']
+
+/**
+ * Default set of files to ignore
+ */
+export const IGNORE_LIST = [
+  'eslint.config.js',
+  'eslint.config.ts',
+  '*.min.*',
+  '*.d.ts',
+  'CHANGELOG.md',
+  'dist',
+  'LICENSE*',
+  'output',
+  'coverage',
+  'temp',
+  'build',
+  'dist',
+  'public/assets',
+  'pnpm-lock.yaml',
+  'yarn.lock',
+  'package-lock.json',
+  '__snapshots__',
+  '!.github',
+  '!.vscode',
+]
+
+/**
+ * Default set of plugins to apply to the config
+ */
+export const PLUGINS_LIST = {
+  '@unicorn': unicorn,
+  '@stylistic': stylistic,
+  ...eslintPluginPrettierRecommended.plugins
+}
+
+/**
+ * Default list of rules to apply
+ */
+export const RULES_LIST = {
+  'quotes': 'off',
+  'indent': 'off',
+  'space-before-function-paren': 'off',
+  'no-irregular-whitespace': ['error'],
+  'no-multiple-empty-lines': ['error', { max: 1 }],
+  'one-var': ['error', 'never'],
+  'eol-last': ['error', 'always'],
+  'no-cond-assign': ['error', 'except-parens'],
+  'comma-dangle': ['error', 'always-multiline'],
+  'eqeqeq': ['error', 'always'],
+  'new-parens': ['error', 'always'],
+  'no-caller': ['error'],
+  'no-constant-condition': ['error'],
+  'no-control-regex': ['error'],
+  'no-debugger': ['error'],
+  'no-duplicate-case': ['error'],
+  'no-eval': ['error'],
+  'no-ex-assign': ['error'],
+  'no-extra-boolean-cast': ['error'],
+  'no-fallthrough': ['error'],
+  'no-inner-declarations': ['error'],
+  'no-invalid-regexp': ['error', { allowConstructorFlags: ['u', 'y'] }],
+  'no-proto': ['error'],
+  'no-shadow': ['off'],
+  'no-regex-spaces': ['error'],
+  'no-self-compare': ['error'],
+  'no-sparse-arrays': ['error'],
+  'no-mixed-spaces-and-tabs': ['error'],
+  'no-unsafe-negation': ['error'],
+  'no-new-wrappers': ['error'],
+  'no-self-assign': ['error'],
+  'no-this-before-super': ['error'],
+  'no-with': ['error'],
+  'rest-spread-spacing': ['error', 'never'],
+  'no-trailing-spaces': ['error', { ignoreComments: true }],
+  'no-undef-init': ['error'],
+  'no-unsafe-finally': ['error'],
+  'padded-blocks': ['error', 'never'],
+  'space-in-parens': ['error', 'never'],
+  'use-isnan': ['error'],
+  'valid-typeof': ['error', { requireStringLiterals: true }],
+  'brace-style': ['error', '1tbs'],
+  'curly': ['error', 'all'],
+  'handle-callback-err': ['error', '^(err|error)$'],
+  'max-len': [
+    'error',
+    {
+      code: 100,
+      comments: 120,
+      ignoreUrls: true,
+      ignoreTemplateLiterals: true,
+    },
+  ],
+  'no-array-constructor': ['error'],
+  'no-unreachable': ['error'],
+  'no-multi-spaces': ['error'],
+
+  '@stylistic/semi': [2, 'never'],
+  '@stylistic/quotes': ['error', 'single'],
+  '@stylistic/space-before-function-paren': ['error', 'always'],
+  '@stylistic/indent': [
+    'error',
+    2,
+    {
+      SwitchCase: 1,
+      flatTernaryExpressions: false,
+      ignoredNodes: ['TSTypeParameterInstantiation'],
+    },
+  ],
+
+  '@typescript-eslint/no-shadow': 'error',
+  '@typescript-eslint/naming-convention': [
+    'error',
+    {
+      selector: 'variable',
+      format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+    },
+    {
+      selector: 'typeLike',
+      format: ['PascalCase'],
+    },
+    {
+      selector: 'class',
+      format: ['PascalCase'],
+    },
+    {
+      selector: 'interface',
+      format: ['PascalCase'],
+      custom: {
+        regex: '^I[A-Z]',
+        match: false,
+      },
+    },
+  ],
+
+  '@unicorn/prefer-module': 'error',
+  '@unicorn/prefer-node-protocol': 'error',
+  '@unicorn/filename-case': [
+    'error',
+    {
+      case: 'snakeCase',
+    },
+  ],
+  '@unicorn/no-await-expression-member': 'error',
+  '@unicorn/no-for-loop': 'error',
+  '@unicorn/no-instanceof-array': 'error',
+  '@unicorn/prefer-number-properties': 'error',
+  ...eslintPluginPrettierRecommended.rules,
+  'prettier/prettier': ['error', { endOfLine: 'auto' }],
+}
+
+/**
+ * Configures ESLint to use an opinionated config tailored for
+ * creating a TypeScript library.
+ * 
+ * You may pass additional config blocks as multiple
+ * arguments to this function.
+ * 
+ * @example
+ * ```js
+ * configPkg()
+ * 
+ * configPkg({
+ *   files: INCLUDE_LIST,
+ *   ignore: IGNORE_LIST,
+ *   rules: {
+ *   }
+ * })
+ * ```
+ */
+export function configPkg(...configBlocksToMerge) {
+  return tseslint.config(
+    tseslint.configs.base,
+    {
+      name: 'Plugins list',
+      plugins: PLUGINS_LIST,
+    },
+    {
+      name: 'AdonisJS pkg defaults',
+      files: INCLUDE_LIST,
+      ignores: IGNORE_LIST,
+      rules: RULES_LIST,
+    },
+    ...configBlocksToMerge
+  )
+}
+
+/**
+ * Configures ESLint to use an opinionated config tailored for
+ * an AdonisJS application
+ * 
+ * You may pass additional config blocks as multiple
+ * arguments to this function.
+ * 
+ * @example
+ * ```js
+ * configApp()
+ * 
+ * configApp({
+ *   files: INCLUDE_LIST,
+ *   ignore: IGNORE_LIST,
+ *   rules: {
+ *   }
+ * })
+ * ```
+ */
+export function configApp(...configBlocksToMerge) {
+  return tseslint.config(
+    tseslint.configs.base,
+    {
+      name: 'Plugins list',
+      plugins: PLUGINS_LIST,
+    },
+    {
+      name: 'AdonisJS app defaults',
+      files: INCLUDE_LIST,
+      ignores: IGNORE_LIST,
+      rules: RULES_LIST,
+    },
+    ...configBlocksToMerge
+  )
+}
